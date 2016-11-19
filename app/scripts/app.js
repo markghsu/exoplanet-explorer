88ec34ec1308d7e1840fa47572ea00a08dc22322
Mark Hsu <mark.h@canelasoftware.com>
2016-11-19 13:22:32 -0800

    add promise to load planet data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,6 +64,16 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-resultsd.json').then(function(response){
+      addSearchHeader(response.query);
+      return getJSON(response.results[0]);
+    }).catch(function(error){
+      throw Error("Search Request Error");
+    }).then(function(response){
+      createPlanetThumb(response);
+    }).catch(function(error){
+      console.log(error);
+      addSearchHeader("unknown");
+    });
   });
 })(document);
